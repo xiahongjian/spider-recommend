3fbffd614d9a4dcd8c757dbf825cd69e8edd3323
xiahongjian <hongjian.xia@qq.com>
2019-12-21 22:39:13 +0800

    添加测试接口

--- src/main/java/tech/hongjian/spider/recommend/controller/IndexController.java
+++ src/main/java/tech/hongjian/spider/recommend/controller/IndexController.java
@@ -1,20 +1,13 @@
 package tech.hongjian.spider.recommend.controller;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import tech.hongjian.spider.recommend.entity.Actor;
-import tech.hongjian.spider.recommend.entity.Recommend;
-import tech.hongjian.spider.recommend.entity.Video;
-import tech.hongjian.spider.recommend.entity.enums.Platform;
-import tech.hongjian.spider.recommend.entity.enums.VideoType;
 import tech.hongjian.spider.recommend.model.RestResponse;
-import tech.hongjian.spider.recommend.parser.IQiYiRecommendParser;
-import tech.hongjian.spider.recommend.parser.QQRecommendParser;
-import tech.hongjian.spider.recommend.parser.YoukuRecommendParser;
+import tech.hongjian.spider.recommend.task.RecommendSpiderTask;
 
 /** 
  * @author xiahongjian
@@ -22,38 +15,21 @@ import tech.hongjian.spider.recommend.parser.YoukuRecommendParser;
  */
 @RestController
 public class IndexController {
-
-    @Autowired
-    private QQRecommendParser qqRecommendParser;
+    @Value("${spider.user}")
+    private String user;
+    @Value("${spider.password}")
+    private String password;
     
     @Autowired
-    private IQiYiRecommendParser iQiYiRecommandParser;
+    private RecommendSpiderTask task;
     
-    @Autowired
-    private YoukuRecommendParser youKuRecommendParser;
     
-//    @GetMapping("/create")
-    public RestResponse<?> test() {
-        Actor he = new Actor();
-        he.setName("何炅");
-        Video video = new Video();
-        video.setActors(Stream.of(he).collect(Collectors.toList()));
-        video.setName("Test name");
-        video.setType(VideoType.VARIETY_SHOW);
-        video.setDescription("desc");
-        
-        Recommend recommend = new Recommend();
-        recommend.setPlatform(Platform.QQ);
-        recommend.setVideo(video);
-        
-        return RestResponse.ok("done");
-    }
-    
-//    @GetMapping("/parse")
-    public RestResponse<?> parse() {
-//        qqRecommendParser.parse();
-//        iQiYiRecommandParser.parse();
-        youKuRecommendParser.parse();
-        return RestResponse.ok("done");
+    @GetMapping("/parse")
+    public RestResponse<?> parse(String user, String password) {
+        if (StringUtils.equals(this.user, user) && StringUtils.equals(this.password, password)) {
+            task.doParse();
+            return RestResponse.ok("done");
+        }
+        return RestResponse.fail("No permission.");
     }
 }
